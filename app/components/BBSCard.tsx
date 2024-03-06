113af38e18001af9bd9504f33090ee4e41ff4be0
tube22-ex <fodata@outlook.jp>
2024-03-07 01:13:33 +0900

    no message

---
+++ app/components/BBSCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+//import { Link } from "next/link";
+import Link from "next/link";
+import { BBSData } from '../types/types';
+interface BBSDataProps{
+    bbsData: BBSData;
+}
+const BBSCard = ({bbsData} : BBSDataProps) => {
+    const {id, title, content, createdAt, username} = bbsData;
+  return (
+    <Card>
+    <CardHeader>
+      <CardTitle>{title}</CardTitle>
+      <CardDescription>{username}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      {content}
+    </CardContent>
+    <CardFooter className="flex justify-between">
+      <Link href={`/bbs-posts/${id}`}>Read More</Link>
+    </CardFooter>
+  </Card>
+  )
+}
+
+export default BBSCard
